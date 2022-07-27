e2626297ad5b03b8e50011dc3c5536597a27c4b3
Saravanan M <sarvan.m20@gmail.com>
2022-07-27 10:23:54 +0530

    Auth on progress

---
+++ backend/Controllers/userControllers.js
@@ -0,0 +1,60 @@
+const asyncHandler = require("express-async-handler");
+const generateToken = require("../Config/generateToken");
+const User = require("../Models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the fields");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exixts");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await User.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid user or password");
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,19 @@ const express = require('express');
 const { chats } = require('./data/data');
 const dotenv = require('dotenv');
 const connectDB = require('./Config/db');
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("API is running successfully");
 })
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-})
-
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id) 
-    res.send(singleChat);
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
